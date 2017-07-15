af0400a2eb2715bc933be64037fc0637063ed379
Mohamed Warsame <mwarsame9@gmail.com>
2017-07-15 14:09:01 -0700

    add admin to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,28 +2,28 @@ Product.destroy_all
 Review.destroy_all
 
 
-# 2.times do |i|
-#   user = User.create!(
-#     email: Faker::Internet.email,
-#     password: Faker::Internet.password(6)
-#   )
+1.times do |i|
+  user = User.create!(
+    email: 'mohamed@gmail.com',
+    password: 'mohamed',
+    admin: true
+  )
   15.times do |i|
    product = Product.create!(
      name: Faker::Coffee.blend_name,
      description: Faker::Coffee.notes,
      price: Faker::Number.between(5, 15),
-     user_id: Faker::Number.between(1,2)
+     user_id: 1
      )
-  #  3.times do |i|
-  #    review = product.reviews.create!(
-  #        content: Faker::TwinPeaks.quote,
-  #        user_id: Faker::Number.between(1, 2)
-  #        )
-  #  end
+   3.times do |i|
+     review = product.reviews.create!(
+         content: Faker::TwinPeaks.quote,
+         user_id: 1
+         )
+   end
   end
-# end
+end
 
+p "Created #{User.count} users"
 p "Created #{Product.count} products"
-
-# once initially set up/to edit must run 'rake db:setup'
-# also 'rake db:test:prepare'
+p "Created #{Reviews.count} reviews"
